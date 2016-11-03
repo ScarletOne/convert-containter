ab1bc4ee5ebe896a2fb4ac1ba7a850762b7253a3
lysek <matt.lysek@gmail.com>
2016-11-03 13:27:21 +0100

    added dummy tests

---
+++ UT/convertUT.cpp
@@ -0,0 +1,18 @@
+#define CATCH_CONFIG_MAIN
+#include "LIBS/catch/catch.hpp"
+
+
+unsigned int dummyFunc()
+{
+    return 1;
+}
+
+TEST_CASE("just a dummy passing test", "[dummy]")
+{
+    REQUIRE(dummyFunc() == 1);
+}
+
+TEST_CASE("just a dummy failing test", "[dummy]")
+{
+    REQUIRE(dummyFunc() == 2);
+}
