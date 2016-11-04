54eb6967525c0c548fb8554d52f6c2c6adeb5e33
lysek <matt.lysek@gmail.com>
2016-11-04 11:00:59 +0100

    added conversion from basic type to a collection - may be redundant but still wanted to check the UT suite

--- SRC/convert.hpp
+++ SRC/convert.hpp
@@ -1,12 +1,39 @@
 #pragma once
 
 #include <algorithm>
+#include <utility>
+#include <iterator>
 
-template<typename OutContainer, typename InContainer>
-OutContainer convert(const InContainer& inContainer)
+template <typename Container>
+struct IsContainer
 {
-    OutContainer outContainer;
-    for(item : inContainer)
+    typedef char yes[1];
+    typedef char no[2];
+
+    template <typename Class>
+    static yes& check(decltype(&Class::size));
+
+    template <typename>
+    static no& check(...);
+
+    static const bool isContainer = sizeof(check<Container>(0))==sizeof(yes);
+};
+
+template<typename Out, typename In>
+typename std::enable_if<!IsContainer<In>::isContainer, Out>::type
+convert(const In& in)
+{
+    Out outContainer;
+    outContainer.insert(std::end(outContainer), in);
+    return outContainer;
+}
+
+template<typename Out, typename In>
+typename std::enable_if<IsContainer<In>::isContainer, Out>::type
+convert(const In& inContainer)
+{
+    Out outContainer;
+    for(auto item : inContainer)
     {
         outContainer.insert(std::end(outContainer), item);
     }
--- UT/convertUT.cpp
+++ UT/convertUT.cpp
@@ -6,21 +6,28 @@
 #include <vector>
 #include <set>
 
-TEST_CASE("convert std::vector<int> to std::vector<int>", "[one dimensional]")
+TEST_CASE("should convert std::vector<int> to std::vector<int>", "[one dimensional]")
 {
     std::vector<int> test_vector = { 1,2,3 };
     REQUIRE(test_vector == convert<std::vector<int>>(test_vector));
 }
 
-TEST_CASE("convert std::vector<std::string> to std::vector<std::string>", "[one dimensional]")
+TEST_CASE("should convert std::vector<std::string> to std::vector<std::string>", "[one dimensional]")
 {
     std::vector<std::string> test_vector = { "Obi", "-", "wan", "Kenobi" };
     REQUIRE(test_vector == convert<std::vector<std::string>>(test_vector));
 }
 
-TEST_CASE("convert std::vector<int> to std::set<int>", "[one dimensional]")
+TEST_CASE("should convert std::vector<int> to std::set<int>", "[one dimensional]")
 {
     std::vector<int> test_vector = { 1,2,3 };
     std::set<int> test_set = { 1,2,3 };
     REQUIRE(test_set == convert<std::set<int>>(test_vector));
+}
+
+TEST_CASE("should convert int to vector<int>", "[base type deduction]")
+{
+    std::vector<int> required_output = { 1 };
+    int input = 1;
+    REQUIRE(required_output == convert<std::vector<int>>(input));
 }
