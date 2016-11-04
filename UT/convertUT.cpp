d6ade645ace4931bddf9da9642d3d9a98125c0eb
lysek <matt.lysek@gmail.com>
2016-11-04 09:42:55 +0100

    removed dummy test

--- UT/convertUT.cpp
+++ UT/convertUT.cpp
@@ -3,16 +3,6 @@
 #include "../SRC/convert.hpp"
 #include <vector>
 
-unsigned int dummyFunc()
-{
-    return 1;
-}
-
-TEST_CASE("just a dummy passing test", "[dummy]")
-{
-    REQUIRE(dummyFunc() == 1);
-}
-
 TEST_CASE("convert vector to vector", "[vector]")
 {
     std::vector<int> v1 = {1,2,3};
